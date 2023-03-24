56ba00cab6672494c1ed57766d76448dc6fa7e06
Huy <Huy@1234>
2023-03-24 21:59:24 +0700

    topcv

--- scrape/main.py
+++ scrape/main.py
@@ -3,29 +3,19 @@ import requests
 from selenium import webdriver
 import time
 from selenium.webdriver.common.keys import Keys
-import webbrowser
-# URL to scrape
-
-# driver = webdriver.Chrome()
-
-chrome_path = "C:\Program Files\Google\Chrome\Application\chrome.exe"
-
-
-
-
-# Get the HTML content
 
 
 def Find_Jobs_In_TopCV():
+    url = "https://www.topcv.vn/"
     search = input("Enter your job: ")
     search = search.strip().replace(" ", "-")
-    url = "https://www.topcv.vn/tim-viec-lam-"+search+"?page=1"
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    driver = webdriver.Chrome(options=options)
 
-    # driver.get(url)
-    webbrowser.register('chrome',
-                        None,
-                        webbrowser.BackgroundBrowser(chrome_path))
-    webbrowser.get('chrome').open(url)
+    url = url + "tim-viec-lam-"+search+"?page=1"
+
+    driver.get(url)
 
     html_text = requests.get(url).text
     soup = BeautifulSoup(html_text, 'lxml')
@@ -52,6 +42,3 @@ if __name__ == '__main__':
         time_wait = 1
         print(f"Waiting {time_wait} minutes...")
         time.sleep(time_wait * 60)
-
-        
-
