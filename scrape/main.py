d0405c9263a9ea3ae702f92f802adf31749beea3
Huy <Huy@1234>
2023-03-24 21:38:35 +0700

    topcv

--- scrape/main.py
+++ scrape/main.py
@@ -1,49 +1,57 @@
 from bs4 import BeautifulSoup
 import requests
+from selenium import webdriver
 import time
+from selenium.webdriver.common.keys import Keys
+import webbrowser
 # URL to scrape
-url = "https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=react+native&txtLocation="
+
+# driver = webdriver.Chrome()
+
+chrome_path = "C:\Program Files\Google\Chrome\Application\chrome.exe"
+
+
+
 
 # Get the HTML content
-def find_jobs():
+
+
+def Find_Jobs_In_TopCV():
+    search = input("Enter your job: ")
+    search = search.strip().replace(" ", "-")
+    url = "https://www.topcv.vn/tim-viec-lam-"+search+"?page=1"
+
+    # driver.get(url)
+    webbrowser.register('chrome',
+                        None,
+                        webbrowser.BackgroundBrowser(chrome_path))
+    webbrowser.get('chrome').open(url)
+
     html_text = requests.get(url).text
     soup = BeautifulSoup(html_text, 'lxml')
-    jobs = soup.find_all('li', class_ ='clearfix job-bx wht-shd-bx')
-    arrJobs = []
 
-    # Loop through the jobs
-    for index, job in enumerate(jobs):
-        published_date = job.find('span', class_ = 'sim-posted').text.strip()
-        if('few' in published_date):
-            company_name = job.find('h3', class_ = 'joblist-comp-name').text.strip()
-            skills = job.find('span', class_ = 'srp-skills').text.strip().replace(' ', '').replace(',',', ')
-            more_info = job.header.h2.a['href']
+    jobs = soup.find_all('div', class_='job-item-default job-ta')
+    jobs_highlight = soup.find_all(
+        'div', class_='job-item-default bg-highlight job-ta')
 
-            arrJobs.append([
-                'Company: ' + company_name,
-                'Required Skills: ' + skills,
-                'More Info: ' + more_info,
-                'Published Date: ' + published_date,
-                "Created at: " + time.ctime()
-            ])
-    
-    if(len(arrJobs) > 0):
-        with open(f'posts/jobs.txt','w') as f:
-            f.write(f"Total Jobs Found: {len(arrJobs)} \n")
-            f.write(f"{'-' * 20} \n")
-            for job in arrJobs:
-                f.write(f"{job[0]} \n")
-                f.write(f"{job[1]} \n")
-                f.write(f"{job[2]} \n")
-                f.write(f"{job[3]} \n")
-                f.write(f"{job[4]} \n")
-                f.write(f"{'-' * 50} \n")
-    else:
-        print('No new jobs found.')
+    if (len(jobs) > 0):
+        linkJob = jobs[0].find('a').get('href')
+        companyJob = jobs[0].find('a', class_='company').text
+        print(f'- Link: {linkJob} \n- Company: {companyJob}')
+    if (len(jobs_highlight) > 0):
+        linkJob_highlight = jobs_highlight[0].find('a').get('href')
+        companyJob_highlight = jobs_highlight[0].find(
+            'a', class_='company').text
+        print(
+            f'- Link: {linkJob_highlight} \n- Company: {companyJob_highlight}')
+
 
 if __name__ == '__main__':
     while True:
-        find_jobs()
-        time_wait = 10
+        Find_Jobs_In_TopCV()
+        time_wait = 1
         print(f"Waiting {time_wait} minutes...")
         time.sleep(time_wait * 60)
+
+        
+
