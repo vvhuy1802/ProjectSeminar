9fc01d423904a71bfbe9029a2d3e1427254a5a0f
nguyendinhduy13 <your_email@example.com>
2023-04-05 23:27:30 +0700

    responsive

--- career-web-ui/src/components/Detail_Jobs.js
+++ career-web-ui/src/components/Detail_Jobs.js
@@ -40,7 +40,7 @@ const Detail_Jobs = () => {
           {window.scrollTo(0, 0)}
           <div className="flex">
             <div>
-              <div className="mx-10 py-8 bg-greyIsh">
+              <div className="mx-10 py-8 bg-greyIsh hidden lg:block">
                 <div className="flex justify-between bg-white shadow-lg shadow-greyIsh-700 w-full items-center p-5">
                   <div className="flex items-center gap-16">
                     <div>
@@ -62,6 +62,35 @@ const Detail_Jobs = () => {
                   <div>
                     <button className=" mr-2 bg-blueColor p-2 w-[200px] flex items-center justify-center gap-3 rounded-[5px] ease-in-out transform hover:-translate-y-1 hover:scale-105 transition duration-500">
                       <BsSend className="text-white text-2xl" />
+                      <span className="text-white text-sm font-medium" onClick={()=>applyNow(item.link_job)}>
+                        ỨNG TUYỂN NGAY
+                      </span>
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className="mx-10 py-8 bg-greyIsh lg:hidden">
+                <div className="flex justify-between bg-white shadow-lg shadow-greyIsh-700 w-full items-center p-5">
+                  <div className="flex items-center gap-5">
+                    <div>
+                      <img
+                        className=" h-[70px] w-[100px]"
+                        src={item.image}
+                        alt="My Image"
+                      />
+                    </div>
+                    <div>
+                      <h1 className=" font-semibold text-blueColor text-[18px]">
+                        {item.job_name}
+                      </h1>
+                      <h2 className=" font-medium text-[16px]">
+                        {item.company_name}
+                      </h2>
+                    </div>
+                  </div>
+                  <div>
+                    <button className=" ml-3 bg-blueColor p-2 w-[170px] flex items-center justify-center gap-3 rounded-[5px] ease-in-out transform hover:-translate-y-1 hover:scale-105 transition duration-500">
+                      <BsSend className="text-white text-[15px]" />
                       <span className="text-white text-sm font-medium" onClick={()=>applyNow(item.link_job)}>
                         ỨNG TUYỂN NGAY
                       </span>
@@ -91,9 +120,6 @@ const Detail_Jobs = () => {
                   <div className="flex gap-1 items-center">
                     <SlLocationPin className=" text-xl" />
                     <span className="text-sm">{item.location}</span>
-                    <button className=" mb-1 ml-2">
-                      <span className=" text-xs underline">See map</span>
-                    </button>
                   </div>
                   <div className="border mt-2" />
                   {item.description &&
@@ -116,7 +142,30 @@ const Detail_Jobs = () => {
                     ))}
                 </div>
               </div>
-
+              <div className="lg:hidden">
+              <div className="px-10 py-4 bg-greyIsh sticky top-0">
+                <div className="bg-white flex gap-2 shadow-lg shadow-greyIsh-700 w-full p-5 items-center">
+                <div className="gap-4 flex items-center">
+                  <img
+                    className="w-[70px] h-[70px]"
+                    src={item.image}
+                    alt="Company"
+                  />
+                  <div>
+                  <p className=" font-semibold text-[17px]">{item.company_name}</p>
+                  <p className=" text-sm mt-2">
+                    {item.company_description.length>100?item.company_description.slice(0,100)+"...":item.company_description}
+                  </p>
+                  </div>
+                  </div>
+                  <button className=" h-12 bg-blueColor rounded-[5px] ease-in-out transform hover:-translate-y-1 hover:scale-105 transition duration-500" onClick={()=>{applyNow(item.link_company)}}>
+                    <span className="text-[14px] text-white">
+                      Xem trang công ty của chúng tôi
+                    </span>
+                  </button>
+                </div>
+              </div>
+            </div>
               <div className="px-10 py-8 bg-greyIsh">
                 <div className="bg-white flex flex-col gap-2 shadow-lg shadow-greyIsh-700 w-full p-5">
                   <span className=" font-semibold text-[20px] text-color_text">
@@ -155,7 +204,7 @@ const Detail_Jobs = () => {
                 </div>
               </div>
             </div>
-            <div >
+            <div className="hidden lg:block" >
               <div className="w-[400px] py-8 bg-greyIsh sticky top-0 mx-5">
                 <div className="bg-white flex flex-col gap-2 shadow-lg shadow-greyIsh-700 w-full p-5 items-center">
                   <img
